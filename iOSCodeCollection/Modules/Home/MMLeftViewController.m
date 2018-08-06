98a76c70f9a4468b6d7bcdf292510452a0871e04
chensongqi <chensongqi@zhongan.com>
2018-08-06 12:01:02 +0800

    点击返回

--- iOSCodeCollection/Modules/Home/MMLeftViewController.m
+++ iOSCodeCollection/Modules/Home/MMLeftViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "MMLeftViewController.h"
+#import "MMCenterViewController.h"
+#import "MMNavigationController.h"
+@interface MMLeftViewController () <UITableViewDelegate, UITableViewDataSource>
 
-@interface MMLeftViewController ()
+@property (nonatomic, strong) UITableView *mainTableView;
+@property (nonatomic, strong) UIColor *bgColor;
 
 @end
 
@@ -16,11 +20,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
+    [self initMainTableView];
 
 }
+- (void)initMainTableView {
+    self.mainTableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    self.mainTableView.delegate = self;
+    self.mainTableView.dataSource = self;
+    UIColor * tableViewBackgroundColor;
+    tableViewBackgroundColor = [UIColor colorWithRed:110.0/255.0
+                                               green:113.0/255.0
+                                                blue:115.0/255.0
+                                               alpha:1.0];
+    self.bgColor = tableViewBackgroundColor;
+    [self.mainTableView setBackgroundColor:tableViewBackgroundColor];
+    [self.view addSubview:self.mainTableView];
+}
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellID = @"leftCellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    cell.backgroundColor = [UIColor clearColor];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.textLabel.text = @"哈哈";
+    return cell;
+}
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    MMCenterViewController * center = [[MMCenterViewController alloc] init];
+    center.view.backgroundColor = [UIColor redColor];
+    UINavigationController * nav = [[MMNavigationController alloc] initWithRootViewController:center];
+    [self.mm_drawerController setCenterViewController:nav withCloseAnimation:YES completion:nil];
+}
 
 @end
