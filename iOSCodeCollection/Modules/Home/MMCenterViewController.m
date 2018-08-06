b2d86b8f182c0b7e83cf4e0d01a78fda1bb49c67
chensongqi <chensongqi@zhongan.com>
2018-08-06 11:22:16 +0800

    代码完善

--- iOSCodeCollection/Modules/Home/MMCenterViewController.m
+++ iOSCodeCollection/Modules/Home/MMCenterViewController.m
@@ -8,30 +8,30 @@
 
 #import "MMCenterViewController.h"
 
+
 @interface MMCenterViewController ()
 
 @end
 
 @implementation MMCenterViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self setupLeftMenuButton];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)setupLeftMenuButton{
+    MMDrawerBarButtonItem * leftDrawerButton = [[MMDrawerBarButtonItem alloc] initWithTarget:self action:@selector(leftDrawerButtonPress:)];
+    [self.navigationItem setLeftBarButtonItem:leftDrawerButton animated:YES];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - Button Handlers
+-(void)leftDrawerButtonPress:(id)sender{
+    [self.mm_drawerController toggleDrawerSide:MMDrawerSideLeft animated:YES completion:nil];
 }
-*/
 
 @end
